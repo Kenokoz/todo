d8f6d46de43195e5b687585528b7ab0f33843a4e
Kirill Andreev <kenokoz@yandex.ru>
2021-04-16 13:57:40 +0300

    Add item deletion

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../appHeader/appHeader';
 import SearchPanel from '../searchPanel/searchPanel';
 import ItemStatusFilter from '../itemStatusFilter/itemStatusFilter';
@@ -6,24 +6,39 @@ import TodoList from '../todoList/todoList';
 
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ];
+class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 },
+    ],
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  handleDeleteItem = id => {
+    const todos = [...this.state.todoData];
+    const index = todos.findIndex(item => item.id === id);
+    todos.splice(index, 1);
+
+    this.setState({ todoData: todos });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={this.handleDeleteItem}
+        />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
--- src/components/todoListItem/todoListItem.js
+++ src/components/todoListItem/todoListItem.js
@@ -1,34 +1,56 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './todoListItem.css';
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
+class TodoListItem extends Component {
+  state = {
+    done: false,
+    important: false,
   };
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
+  handleLableClick = () => {
+    this.setState(({ done }) => {
+      return { done: !done };
+    });
+  };
+
+  handleMarkImportant = () => {
+    this.setState(({ important }) => {
+      return { important: !important };
+    });
+  };
+
+  render() {
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
+
+    let classes = 'todo-list-item';
+    classes += done ? ' done' : '';
+    classes += important ? ' important' : '';
+
+    return (
+      <span className={classes}>
+        <span className="todo-list-item-label" onClick={this.handleLableClick}>
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.handleMarkImportant}
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button
-        type="button"
-        className="btn btn-outline-success btn-sm float-right"
-      >
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button
-        type="button"
-        className="btn btn-outline-danger btn-sm float-right"
-      >
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+    );
+  }
+}
 
 export default TodoListItem;
